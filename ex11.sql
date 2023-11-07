045b71cb062eb048b1e660a41327bf6188860f36
GabrielToth <gabt71@gmail.com>
2023-11-07 14:37:59 +0100

    ex11

---
+++ ex11.sql
@@ -0,0 +1,6 @@
+/*Requête 11. Lister les entités et leurs intermédiaires associés :*/
+
+SELECT e.name AS EntityName, i.name AS IntermediaryName
+FROM entity e
+JOIN assoc_inter_entity aie ON e.id = aie.entity
+JOIN intermediary i ON aie.inter = i.id;
